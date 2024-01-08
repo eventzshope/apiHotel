8a79046161ca6b6fbbd32b25fe6a5ec89ec3b935
eventzshope <98055568+eventzshope@users.noreply.github.com>
2024-01-08 13:17:44 -0800

    Update sequelize.js

--- db/sequelize.js
+++ db/sequelize.js
@@ -3,10 +3,13 @@ const User = require("../models/User");
 const CHAMBRE=require('../models/chambre')
 const RESERVATION=require('../models/Reservation')
 
-const sequelize = new Sequelize("if0_35451994_API", "if0_35451994","vuuH8pNIoM8yVL", {
-  host: "sql210.infinityfree.com",
-  dialect: "mariadb",
-  logging: false,
+const sequelize = new Sequelize({
+   dialect: 'mariadb',
+  host: 'sql210.infinityfree.com',
+  port: 3306, // Le port MySQL par défaut
+  username: 'if0_35451994',
+  password: 'vuuH8pNIoM8yVL', // Remplacez par votre mot de passe vPanel
+  database: 'if0_35451994_API',
 });
 
 sequelize
