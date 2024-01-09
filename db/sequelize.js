50b5cb3395865ac57b8615906b637fe2b44c66f7
eventzshope <98055568+eventzshope@users.noreply.github.com>
2024-01-09 11:42:57 -0800

    Add files via upload

--- db/sequelize.js
+++ db/sequelize.js
@@ -2,15 +2,13 @@ const { DataTypes, Sequelize } = require("sequelize");
 const User = require("../models/User");
 const CHAMBRE=require('../models/chambre')
 const RESERVATION=require('../models/Reservation')
-
-const sequelize = new Sequelize("bql3rhfld479jnsds3kj", "uiaamrqiqazzm6hs", "bql3rhfld479jnsds3kj", {
-  host: "bql3rhfld479jnsds3kj-mysql.services.clever-cloud.com",
-  port: 3306,
-  dialect: "mysql",
-  logging: false,
+require("dotenv").config()
+const sequelize = new Sequelize(process.env.DBNAME, process.env.DB_USERNAME, process.env.DBPASSWORD, {
+  host: process.env.DB_HOST,
+  dialect:"mysql",
 });
 
-sequelize
+sequelize 
   .authenticate()
   .then((_) => "connexion reussir")
   .catch((error) => console.log(error));
